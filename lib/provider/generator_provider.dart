8e0247ae4a7558bd48a4c92f42d72d7ec053ba19
fauzirramadan <fauzirramadan@gmail.com>
2022-12-13 21:40:12 +0700

    update share image

--- lib/provider/generator_provider.dart
+++ lib/provider/generator_provider.dart
@@ -1,10 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:meme_generator_app/utils/nav_utils.dart';
+import 'package:meme_generator_app/utils/notif_utils.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:screenshot/screenshot.dart';
+import 'package:share_plus/share_plus.dart';
 
 class GeneratorProvider extends ChangeNotifier {
   bool isEditText = false;
@@ -57,13 +63,30 @@ class GeneratorProvider extends ChangeNotifier {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      actionButton(onTap: () => saveImage(capturedImage)),
-                      actionButton(onTap: () {}, title: "SHARE"),
+                      actionButton(onTap: () async {
+                        await saveImage(capturedImage);
+                        NotificationUtils.showSnackbar(
+                            "BERHASIL MENYIMPAN GAMBAR",
+                            backgroundColor: Colors.green);
+                        Nav.back();
+                      }),
+                      actionButton(
+                        title: "SHARE",
+                        onTap: () => saveAndShare(capturedImage),
+                      ),
                     ],
                   )
                 ],
               ),
             ));
+  }
+
+  Future saveAndShare(Uint8List bytes) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final image = await File('${directory.path}/meme.png').create();
+    image.writeAsBytesSync(bytes);
+
+    await Share.shareXFiles([XFile(image.path)]);
   }
 
   Future<String> saveImage(Uint8List bytes) async {
